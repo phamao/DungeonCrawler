fed752a7f31c711fc7844b7f4d69b223e5980d24
Andrew Pham <phamao@bc.edu>
2023-08-04 19:21:50 -0400

    Serialization with Gson

--- src/main/java/components/Sprite.java
+++ src/main/java/components/Sprite.java
@@ -7,24 +7,29 @@ import java.util.Vector;
 
 public class Sprite {
 
-    private Texture texture;
-    private Vector2f[] texCoords;
-
-    public Sprite(Texture texture) {
-        this.texture = texture;
-        Vector2f[] texCoords = {
+    private Texture texture = null;
+    private Vector2f[] texCoords = {
                 new Vector2f(1, 1),
                 new Vector2f(1, 0),
                 new Vector2f(0, 0),
                 new Vector2f(0, 1)
         };
-        this.texCoords = texCoords;
-    }
 
-    public Sprite(Texture texture, Vector2f[] texCoords) {
-        this.texture = texture;
-        this.texCoords = texCoords;
-    }
+//    public Sprite(Texture texture) {
+//        this.texture = texture;
+//        Vector2f[] texCoords = {
+//                new Vector2f(1, 1),
+//                new Vector2f(1, 0),
+//                new Vector2f(0, 0),
+//                new Vector2f(0, 1)
+//        };
+//        this.texCoords = texCoords;
+//    }
+//
+//    public Sprite(Texture texture, Vector2f[] texCoords) {
+//        this.texture = texture;
+//        this.texCoords = texCoords;
+//    }
 
     public Texture getTexture() {
         return this.texture;
@@ -32,5 +37,13 @@ public class Sprite {
 
     public Vector2f[] getTexCoords() {
         return  this.texCoords;
+    }
+
+    public void setTexture(Texture tex) {
+        this.texture = tex;
+    }
+
+    public void setTexCoords(Vector2f[] texCoords) {
+        this.texCoords = texCoords;
     }
 }
--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -1,5 +1,7 @@
 package jade;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import components.Sprite;
 import components.SpriteRenderer;
 import components.Spritesheet;
@@ -12,6 +14,7 @@ public class LevelEditorScene extends Scene{
 
     private GameObject obj1;
     private Spritesheet sprites;
+    private SpriteRenderer obj1Sprite;
 
     public LevelEditorScene() {
 
@@ -26,15 +29,27 @@ public class LevelEditorScene extends Scene{
         sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
 
         obj1 = new GameObject("Object 1", new Transform(new Vector2f(200, 100), new Vector2f(256, 256)), 2);
-        obj1.addComponent(new SpriteRenderer(new Vector4f(1, 0, 0, 1)));
+        obj1Sprite = new SpriteRenderer();
+        obj1Sprite.setColor(new Vector4f(1, 0, 0, 1));
+        obj1.addComponent(obj1Sprite);
         this.addGameObjectToScene(obj1);
         this.activeGameObject = obj1;
 
         GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)), 1);
-        obj2.addComponent(new SpriteRenderer(new Sprite(
-                AssetPool.getTexture("assets/images/blendImage2.png")
-        )));
+        SpriteRenderer obj2SpriteRenderer = new SpriteRenderer();
+        Sprite obj2Sprite = new Sprite();
+        obj2Sprite.setTexture(AssetPool.getTexture("assets/images/blendImage2.png"));
+        obj2SpriteRenderer.setSprite(obj2Sprite);
+        obj2.addComponent(obj2SpriteRenderer);
         this.addGameObjectToScene(obj2);
+
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        String serialized = gson.toJson(obj1);
+        System.out.println(serialized);
+        GameObject obj = gson.fromJson(serialized, GameObject.class);
+        System.out.println(obj);
     }
 
     private void loadResources() {
