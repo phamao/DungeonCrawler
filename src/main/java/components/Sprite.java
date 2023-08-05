899ce76b2d4b207e02a5ec12be3a11fe0a7d259b
Andrew Pham <phamao@bc.edu>
2023-08-05 13:33:23 -0400

    Converting Screen Coords to World Coords

--- src/main/java/components/Sprite.java
+++ src/main/java/components/Sprite.java
@@ -7,6 +7,8 @@ import java.util.Vector;
 
 public class Sprite {
 
+    private float width, height;
+
     private Texture texture = null;
     private Vector2f[] texCoords = {
                 new Vector2f(1, 1),
@@ -14,22 +16,6 @@ public class Sprite {
                 new Vector2f(0, 0),
                 new Vector2f(0, 1)
         };
-
-//    public Sprite(Texture texture) {
-//        this.texture = texture;
-//        Vector2f[] texCoords = {
-//                new Vector2f(1, 1),
-//                new Vector2f(1, 0),
-//                new Vector2f(0, 0),
-//                new Vector2f(0, 1)
-//        };
-//        this.texCoords = texCoords;
-//    }
-//
-//    public Sprite(Texture texture, Vector2f[] texCoords) {
-//        this.texture = texture;
-//        this.texCoords = texCoords;
-//    }
 
     public Texture getTexture() {
         return this.texture;
@@ -45,5 +31,25 @@ public class Sprite {
 
     public void setTexCoords(Vector2f[] texCoords) {
         this.texCoords = texCoords;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public int getTexId() {
+        return texture == null ? -1 : texture.getId();
     }
 }
